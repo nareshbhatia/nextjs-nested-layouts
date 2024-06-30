7f7d469cabd2bb7ffedd27567470721bf236e150
Naresh Bhatia <me@nareshbhatia.dev>
2024-06-30 02:16:10 -0400

    Write app description in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <p className="py-8">Hello World</p>;
+  return (
+    <div className="py-8">
+      <h1 className="text-2xl font-semibold leading-none tracking-tight">
+        Next.js Nested Layouts
+      </h1>
+      <p className="py-4">
+        This application shows various ways of nesting layouts in Next.js.
+      </p>
+    </div>
+  );
 }
